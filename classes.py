9c618846edabf10d4a5e50ad4d4b791a633cee6c
Amit26922 <amitgollapudi59@gmail.com>
2025-02-10 21:38:10 +0530

    Add files via upload

---
+++ classes.py
@@ -0,0 +1,8 @@
+class Car: #creating a class 
+    make = "Mercedes benz" #declaring and defining class attributes and their variables
+    colour = "black"
+    ModelClass = "S"
+
+car = Car() #creating an object from that created class
+
+print(f"details of the car make->{car.make}, colour ->{car.colour}, model->{car.ModelClass}") #accessing the classing variables directly through the object using dot(.) opeartor
